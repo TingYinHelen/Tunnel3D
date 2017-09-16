64cf0b267801ec8accbb83d19b38294d932175b7
Helen <yinhelen.hlj@qq.com>
2017-09-16 23:03:53 +0800

    Mod: modify the animation

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # 啦啦啦啦，遨游在3D世界的heHENNNNN~~~
-<https://tingyinhelen.github.io/taobaoDouble113D/>
-1
+<https://tingyinhelen.github.io/Tunnel3D/>
--- index.js
+++ index.js
@@ -1,121 +1,64 @@
-var camera, scene, renderer;
-var texture_placeholder,
-isUserInteracting = false,
-onMouseDownMouseX = 0, onMouseDownMouseY = 0,
-lon = 90, onMouseDownLon = 0,
-lat = 0, onMouseDownLat = 0,
-phi = 0, theta = 0,
-target = new THREE.Vector3();
-init();
-animate();
-function init() {
-	var container, mesh;
-	container = document.getElementById( 'container' );
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
-	scene = new THREE.Scene();
-	texture_placeholder = document.createElement( 'canvas' );
-	texture_placeholder.width = 128;
-	texture_placeholder.height = 128;
-	var context = texture_placeholder.getContext( '2d' );
-	context.fillStyle = 'rgb( 200, 200, 200 )';
-	context.fillRect( 0, 0, texture_placeholder.width, texture_placeholder.height );
-	var materials = [
-		loadTexture( 'static/textures/px.jpg' ), // right
-		loadTexture( 'static/textures/nx.jpg' ), // left
-		loadTexture( 'static/textures/py.jpg' ), // top
-		loadTexture( 'static/textures/ny.jpg' ), // bottom
-		loadTexture( 'static/textures/pz.jpg' ), // back
-		loadTexture( 'static/textures/nz.jpg' ) // front
-	];
-	mesh = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 300, 7, 7, 7 ), materials );
-	mesh.scale.x = - 1;
-	scene.add( mesh );
-	for ( var i = 0, l = mesh.geometry.vertices.length; i < l; i ++ ) {
-		var vertex = mesh.geometry.vertices[ i ];
-		vertex.normalize();
-		vertex.multiplyScalar( 550 );
-	}
-	renderer = new THREE.CanvasRenderer();
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
-	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-	document.addEventListener( 'wheel', onDocumentMouseWheel, false );
-	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-	//
-	window.addEventListener( 'resize', onWindowResize, false );
-}
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-}
-function loadTexture( path ) {
-	var texture = new THREE.Texture( texture_placeholder );
-	var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-	var image = new Image();
-	image.onload = function () {
-		texture.image = this;
-		texture.needsUpdate = true;
-	};
-	image.src = path;
-	return material;
-}
-function onDocumentMouseDown( event ) {
-	event.preventDefault();
-	isUserInteracting = true;
-	onPointerDownPointerX = event.clientX;
-	onPointerDownPointerY = event.clientY;
-	onPointerDownLon = lon;
-	onPointerDownLat = lat;
-}
-function onDocumentMouseMove( event ) {
-	if ( isUserInteracting === true ) {
-		lon = ( onPointerDownPointerX - event.clientX ) * 0.1 + onPointerDownLon;
-		lat = ( event.clientY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
-	}
-}
-function onDocumentMouseUp( event ) {
-	isUserInteracting = false;
-}
-function onDocumentMouseWheel( event ) {
-	camera.fov += event.deltaY * 0.05;
-	camera.updateProjectionMatrix();
-}
-function onDocumentTouchStart( event ) {
-	if ( event.touches.length == 1 ) {
-		event.preventDefault();
-		onPointerDownPointerX = event.touches[ 0 ].pageX;
-		onPointerDownPointerY = event.touches[ 0 ].pageY;
-		onPointerDownLon = lon;
-		onPointerDownLat = lat;
-	}
-}
-function onDocumentTouchMove( event ) {
-	if ( event.touches.length == 1 ) {
-		event.preventDefault();
-		lon = ( onPointerDownPointerX - event.touches[0].pageX ) * 0.1 + onPointerDownLon;
-		lat = ( event.touches[0].pageY - onPointerDownPointerY ) * 0.1 + onPointerDownLat;
-	}
-}
-function animate() {
-	requestAnimationFrame( animate );
-	update();
-}
-function update() {
-	if ( isUserInteracting === false ) {
-		lon += 0.1;
-	}
-	lat = Math.max( - 85, Math.min( 85, lat ) );
-	phi = THREE.Math.degToRad( 90 - lat );
-	theta = THREE.Math.degToRad( lon );
-	target.x = 500 * Math.sin( phi ) * Math.cos( theta );
-	target.y = 500 * Math.cos( phi );
-	target.z = 500 * Math.sin( phi ) * Math.sin( theta );
-	camera.position.copy( target ).negate();
-	camera.lookAt( target );
-	renderer.render( scene, camera );
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera.position.set(0, 0, 7);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+let renderer = new THREE.WebGLRenderer({
+		//antialias: true, // 是否进行抗锯齿
+		preserveDrawingBuffer: true // 是否保留缓冲区，直到手动清除或覆盖。
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+// THREEx.WindowResize.bind(renderer, camera);
+// 平行光 橙色
+let light1 = new THREE.DirectionalLight(0xff8000, 0.5);
+light1.position.set(1, 1, 0).normalize();
+scene.add(light1);
+// 平行光 绿色
+let light2 = new THREE.DirectionalLight(0xff8000, 0.5);
+light2.position.set(-1, 1, 0).normalize();
+scene.add(light2);
+// 点光源 青色
+let light3 = new THREE.PointLight(0x44FFAA, 15, 20);
+light3.position.set(0, -3, 0);
+scene.add(light3);
+// 点光源 橙色
+let light4 = new THREE.PointLight(0xff4400, 20, 20);
+light4.position.set(3, 3, 0);
+scene.add(light4);
+// 雾气效果，黑色，雾密度的增长速度 0.15
+scene.fog = new THREE.FogExp2(0x000000, 0.15);
+// 圆柱体模型,顶端半径，底端半径，高度，周长分割面，圆柱体高度分割面，圆柱体两端不覆盖
+let geometry = new THREE.CylinderGeometry(1, 1, 30, 32, 1, true);
+// 贴图
+let loader = new THREE.TextureLoader();
+let texture = loader.load("static/textures/water.jpg");
+// 纹理重复
+texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+// 圆柱体材质
+let material = new THREE.MeshLambertMaterial({
+		color: 0xFFFFFF,
+		map: texture,
+		// 双面
+		side: THREE.DoubleSide
+});
+// 圆柱对象
+let mesh = new THREE.Mesh(geometry, material);
+// X轴旋转 80 度，从圆柱底面往上看
+mesh.rotation.x = -Math.PI / 180 * 80;
+scene.add(mesh);
+// 渲染
+render();
+function render() {
+		// 纹理的位置移动
+		texture.offset.y += 0.008;
+		texture.offset.y %= 1;
+		let seconds = Date.now() / 1000;
+		let radius = 0.9;
+		let angle = Math.sin(0.75 * seconds * Math.PI) / 4;
+		camera.position.x = Math.cos(angle - Math.PI / 2) * radius;
+		camera.position.y = Math.sin(angle - Math.PI / 2) * radius;
+		camera.rotation.z = angle;
+		renderer.render(scene, camera);
+		requestAnimationFrame(render);
 }
